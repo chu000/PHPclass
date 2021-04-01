0cc31bcba2b0051fa8432bcc155daedf4d99261e
chewing0705 <80665219+chewing0705@users.noreply.github.com>
2021-04-01 12:05:42 +0800

    0401

---
+++ test/parts/a20210401-02-page2.php
@@ -0,0 +1,8 @@
+<?php include __DIR__ . '/config.php'; ?>
+<?php
+$title = '我的主頁';
+?>
+<?php include __DIR__ . '/html-head.php'; ?>
+<?php include __DIR__ . '/navbar.php'; ?>
+<?php include __DIR__ . '/scripts.php'; ?>
+<?php include __DIR__ . '/html-foot.php'; ?>
