66be5ee8bc6d073e9f26fc473365128d59651fff
chewing0705 <80665219+chewing0705@users.noreply.github.com>
2021-04-06 11:55:47 +0800

    0406

---
+++ test/a20210406-02-array-json.php
@@ -0,0 +1,29 @@
+<?php
+$persons = [
+    [
+        'name' => 'Bill',
+        'age' => 27,
+        'gender' => 'male',
+    ],
+    [
+        'name' => '大衛',
+        'age' => 25,
+        'gender' => 'male',
+    ],
+    [
+        'name' => 'Flora',
+        'age' => 22,
+        'gender' => 'female',
+    ],
+    [
+        'name' => 'Mary',
+        'age' => 28,
+        'gender' => 'female',
+    ]
+];
+
+// header('Content-Type: application/json');
+echo json_encode([
+    'get' => $_GET,
+    'persons' => $persons
+], JSON_UNESCAPED_UNICODE);
