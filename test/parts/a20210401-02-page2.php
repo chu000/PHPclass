70584045da03542eaaccaf0e62174c96220727ed
chewing0705 <2018g011117@gmail.com>
2021-04-13 12:53:45 +0800

    0413

--- test/parts/a20210401-02-page2.php
+++
@@ -1,8 +0,0 @@
-<?php include __DIR__ . '/config.php'; ?>
-<?php
-$title = '我的主頁';
-?>
-<?php include __DIR__ . '/html-head.php'; ?>
-<?php include __DIR__ . '/navbar.php'; ?>
-<?php include __DIR__ . '/scripts.php'; ?>
-<?php include __DIR__ . '/html-foot.php'; ?>
